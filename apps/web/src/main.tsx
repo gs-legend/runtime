467d03d9e6ec37b51edfcbebd077eee273f96cfd
Sudheer <sudheer.gadhala@kagamierp.com>
2022-02-10 19:04:36 +0530

    sff

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom';
+
+import Root from './containers/Root';
+
+ReactDOM.render(
+  <StrictMode>
+    <Root />
+  </StrictMode>,
+  document.getElementById('root')
+);
